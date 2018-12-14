2dd62f60885d0b765c9974fcab4e9dbf7ade7891
Mario Baier <mario.baier@acp.at>
2018-12-14 18:41:20 +0100

    javascript refactoring + grid layout überarbeitet

--- scripts/main.js
+++ scripts/main.js
@@ -14,54 +14,9 @@ function onLoad() {
     }
 }
 
-function appendNews() {
-    const template = document.querySelector("#newsTemplate");
+function appendTemplateContent(templateId) {
+    const template = document.querySelector("#" + templateId);
         
-    if (template != null) {
-        const main = document.querySelector("main");
-        main.innerHTML = template.innerHTML;
-    }
-}
-
-function appendBio() {
-    const template = document.querySelector("#bioTemplate");
-        
-    if (template != null) {
-        const main = document.querySelector("main");
-        main.innerHTML = template.innerHTML;
-    }
-}
-
-function appendalbum() {
-    const template = document.querySelector("#albumTemplate");
-
-    if (template != null) {
-        const main = document.querySelector("main");
-        main.innerHTML = template.innerHTML;
-    }
-}
-
-function appendaccident() {
-    const template = document.querySelector("#accidentTemplate");
-
-    if (template != null) {
-        const main = document.querySelector("main");
-        main.innerHTML = template.innerHTML;
-    }
-}
-
-function appendfoundation() {
-    const template = document.querySelector("#foundationTemplate");
-
-    if (template != null) {
-        const main = document.querySelector("main");
-        main.innerHTML = template.innerHTML;
-    }
-}
-
-function appendcontact() {
-    const template = document.querySelector("#contactTemplate");
-
     if (template != null) {
         const main = document.querySelector("main");
         main.innerHTML = template.innerHTML;
