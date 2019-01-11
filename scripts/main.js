a90aa4bc34629fdfee7ed3ef333454dea6618941
Mario Baier <mario.baier@acp.at>
2019-01-11 16:13:40 +0100

    added burger button + functionality

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ function templatesAreSupported() {
 
 function onLoad() {
     if (!templatesAreSupported()) {
-        alert("Templates are not supported in your browser");4
+        alert("Templates are not supported in your browser");
     }
 }
 
@@ -20,5 +20,23 @@ function appendTemplateContent(templateId) {
     if (template != null) {
         const main = document.querySelector("main");
         main.innerHTML = template.innerHTML;
+    }
+}
+
+let navigationIsVisible = true;
+function showOrHideNavigation() {
+    const element = document.getElementById("navigation");
+    const mainElement = document.getElementById("main");
+
+    if (navigationIsVisible) {
+        element.classList.add("nav-invisible");
+        mainElement.classList.add("gridlayout-without-nav");
+        navigationIsVisible = false;
+    }
+    else {
+        element.classList.add("nav-visible");
+        element.classList.remove("nav-invisible");
+        mainElement.classList.remove("gridlayout-without-nav");
+        navigationIsVisible = true;
     }
 }
